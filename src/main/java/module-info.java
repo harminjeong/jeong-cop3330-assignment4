47bfd87561225fb7996902fcd00d286ab6741809
Harmin Jeong <harminjeong@gmail.com>
2021-10-31 19:33:37 -0400

    2nd commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module ucf.assignments {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires junit;
+    requires org.junit.jupiter.api;
 
     opens ucf.assignments to javafx.fxml;
     exports ucf.assignments;
---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void onAddButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void descriptionText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void datePicker() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onEditButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onEditTitleButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onDeleteButtonClick() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void onSaveButtonClick() {
+    }
+}
